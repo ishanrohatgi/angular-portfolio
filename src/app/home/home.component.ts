be55500a20ecde298ea8f901ca1cd8ff6b3f8a58
Ishan Rohatgi <ishan.77775@gmail.com>
2023-05-12 12:14:19 +0530

    mario-jump-added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  jumped = false;
 
+ clicked(){
+  this.jumped = true;
+  setTimeout(()=>{
+    this.jumped = false;
+  },1000);
+ }
   constructor() { }
 
   ngOnInit(): void {
