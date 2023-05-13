82c623c59bfd49606c141ca727480ffd1a96afab
Ishan Rohatgi <ishan.77775@gmail.com>
2023-05-13 17:54:06 +0530

    updated intro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 const routes: Routes = [
-  { path: 'about', component: AboutComponent },
+  { path: 'resume', component: AboutComponent },
   { path: '', component: HomeComponent },
 ];
 
