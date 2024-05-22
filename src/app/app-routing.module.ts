01de3c4d1ac95a795c38b1a4d12b7744ec021cc5
Ishan Rohatgi <ishan.77775@gmail.com>
2024-05-23 02:29:01 +0530

    fixed wildcard routing and removed contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
+//import { ContactComponent } from './contact/contact.component';
 const routes: Routes = [
   { path: 'resume', component: AboutComponent },
   { path: '', component: HomeComponent },
   { path: 'projects', component: ProjectsComponent },
+  {path:'**',
+    redirectTo:""
+  }
+  // {path:'contact',component: ContactComponent}
 ];
 
 @NgModule({
